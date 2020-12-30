59c7d3fd5d592123862bf17305ba87f3e19822b8
seungwookjea <69675140+seungwookjea@users.noreply.github.com>
2020-12-30 13:36:09 +0900

    ㅇㅇ

---
+++ Signal_Project_V1/Assets/Script/J_Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Move : MonoBehaviour
+{
+    
+    public float speed = 10;
+    CharacterController cc;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        cc.Move(move * speed * Time.deltaTime);
+    }
+}
